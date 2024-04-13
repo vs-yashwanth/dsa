c2001ebc90cd8608b0a425cda792642f11b2d980
yash <yashwanthvs@gmail.com>
2024-04-13 10:07:32 +0530

    fix min_meeting_rooms

--- algorithms/merge_intervals_min_meeting_rooms.py
+++ algorithms/merge_intervals_min_meeting_rooms.py
@@ -12,9 +12,9 @@ def main(intervals):
 
     for int in intervals:
         int_start, int_end = int
+        pq.heappush(rooms, (int_end, int_start))
         while rooms and int_start >= rooms[0][0]:
             pq.heappop(rooms)
-        pq.heappush(rooms, (int_end, int_start))
         max_rooms = max(max_rooms, len(rooms))
 
     return max_rooms
@@ -24,3 +24,11 @@ print(main([[1, 4], [2, 5], [7, 9]]))
 print(main([[6, 7], [2, 4], [8, 12]]))
 print(main([[1, 4], [2, 3], [3, 6]]))
 print(main([[4, 5], [2, 3], [2, 4], [3, 5]]))
+print(main([(0, 10), (10, 20), (20, 30), (30, 40), (40, 50), (50, 60), (60, 70), (70, 80),
+      (80, 90), (90, 100), (0, 100), (10, 90), (20, 80), (30, 70), (40, 60), (50, 50)]))
+
+# 2
+# 1
+# 2
+# 2
+# 6
